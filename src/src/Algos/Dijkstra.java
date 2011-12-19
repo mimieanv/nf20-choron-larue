d130077065fe2815254434768ce44cfc70d9ea86
cmoidu60@gmail.com <cmoidu60@gmail.com@5812a855-de82-72ee-373f-21f2388d8f56>
2011-12-19 10:50:47 +0000

    *edit Dijkstra.java: bug ligne 56 :S *edit Main.java: rajout du test de dijkstra

--- src/src/Algos/Dijkstra.java
+++ src/src/Algos/Dijkstra.java
@@ -5,46 +5,29 @@ import structure.*;
 
 public class Dijkstra {
 	
-	//private int[] distance;
-	//private int[] sommets;
-	//private int[] pere;
-	
-	ArrayList<String> distance	= new ArrayList<String>();
-	ArrayList<String> sommets	= new ArrayList<String>();
-	ArrayList<Arc> pere;
-	
 	private Graph graphe;
 	int nbArcs;
 	
-/*	public Dijkstra(Graph _graphe) {
+	public Dijkstra(Graph _graphe) {
 		graphe = _graphe;
-		int nbArcs = _graphe.getNbArcs();
-
-		sommets = new ArrayList<String>();
-		
-		// Initialisation des tableaux
-		for (int i=0; i<=nbArcs; i++)
-			distance[i]	= -1;	// -1 <==> + infini
-		distance[1] = 0;
-		pere[1]		= 0;
 	}
-*/
+	
+/*	public Dijkstra(Graph _graphe) {
+	graphe = _graphe;
+	int nbArcs = _graphe.getNbArcs();
 
-/*
-	public int dijkstraAlgoProf(int _s) {
-		for (int i=0; i <= nbArcs; i++) {
-			int min = -1;	// -1 <==> + infini
-			for (int j=0; j <= nbArcs; j++) {
-				if(j)
-			}
-		}
-		
-		return 0;
-	}
+	sommets = new ArrayList<String>();
+	
+	// Initialisation des tableaux
+	for (int i=0; i<=nbArcs; i++)
+		distance[i]	= -1;	// -1 <==> + infini
+	distance[1] = 0;
+	pere[1]		= 0;
+}
 */
 	
-	public ArrayList<Node> dijkstraAlgoWikipedia(Node _startNode, Node _endNode) {
-		for (Node node : graphe.getListNode()) {
+	public ArrayList<Node> algoWikipedia(Node _startNode, Node _endNode) {
+		for(Node node : graphe.getListNode()) {
 	         node.setParcouru(-1);
 	         node.setPrecedent(null);
 		}
@@ -58,7 +41,7 @@ public class Dijkstra {
 			Node node = pasEncoreVu.get(indexNode);
 			pasEncoreVu.remove(indexNode);
 			
-			for (Node fils : node.getFistons()) {
+			for(Node fils : node.getFistons()) {
 				if(fils.getParcouru() > (node.getParcouru() + node.distance(fils))) {
 					fils.setParcouru (node.getParcouru() + node.distance(fils));
 					fils.setPrecedent(node);
@@ -70,36 +53,12 @@ public class Dijkstra {
 		
 		while(lastNode != _startNode) {
 			chemin.add(0, lastNode);
-			lastNode = lastNode.getPrecedent();
+			lastNode = lastNode.getPrecedent();		//TODO BUG ICI !
 		}
 		
 		chemin.add(0, _endNode);
 		
-		return chemin;
-			
-		/*
-	    Tant que pasEncoreVu != liste vide
-	        n1 = minimum(pasEncoreVu)   // Le nœud dans pasEncoreVu avec parcouru le plus petit
-	        pasEncoreVu.enlever(n1)
-	        Pour n2 parcourant fils(n1)   // Les nœuds reliés à n1 par un arc
-	            Si n2.parcouru > n1.parcouru + distance(n1, n2)   // distance correspond au poids de l'arc reliant n1 et n2
-	                n2.parcouru = n1.parcouru + distance(n1, n2)
-	                n2.précédent = n1   // Dit que pour aller à n2, il faut passer par n1
-	            Fin si
-	        Fin pour
-	    Fin tant que
-	    
-	    chemin = liste vide
-	    n = fin
-	    Tant que n != début
-	        chemin.ajouterAvant(n)
-	        n = n.précédent
-	    Fin tant que
-	    chemin.ajouterAvant(fin)
-	    Retourner chemin
-	Fin fonction Dijkstra
-	*/
-		
+		return chemin;		
 	}
 	
 	/**
@@ -108,65 +67,14 @@ public class Dijkstra {
 	 * @return int 
 	 */
 	public int minimum (ArrayList<Node> _listNode) {
-		int i;	// initialiser i
+		int i = Integer.MAX_VALUE;
+		int indexNode = -1;
 		for (Node node : _listNode) {
 			if(node.getParcouru() < i)
 				i = node.getParcouru();
+			indexNode++;
 		}
-		//TODO retourner l'index du node avec i le plus petit
+		return indexNode;
 	}
-	
-	/*
-	// Dijkstra's algorithm to find shortest path from s to all other nodes
-	public static int [] dijkstraAlgo (Graph _graphe, int _s) {
-		
-		int [] dist 		= new int [_graphe.getSize()];  	// shortest known distance from "s"
-		int [] pred			= new int [_graphe.getSize()];  	// preceeding node in path
-		boolean [] visited	= new boolean [_graphe.getSize()]; 	// all false initially
-			  
-		for (int i=0; i<dist.length; i++) {
-			dist[i] = Integer.MAX_VALUE;
-		}
-		dist[_s] = 0;
-		
-		for (int i=0; i<dist.length; i++) {
-			final int next = minVertex (dist, visited);
-			visited[next] = true;
 
-			 // The shortest path to next is dist[next] and via pred[next].
-
-			final int [] n = _graphe.neighbors (next);
-			for (int j=0; j<n.length; j++) {
-				final int v = n[j];
-				final int d = dist[next] + _graphe.getWeight(next,v);
-				if (dist[v] > d) {
-					dist[v] = d;
-					pred[v] = next;
-				}
-			}
-		}
-		return pred;  // (ignore pred[s]==0!)
-	}
-	
-	private void initialize(Graph _graphe, int _s) {
-		final int [] dist = new int [_graphe.getSize()];  // shortest known distance from "s"
-		final int [] pred = new int [_graphe.getSize()];  // preceeding node in path
-		final boolean [] visited = new boolean [_graphe.getSize()]; // all false initially
-			  
-		for (int i=0; i<dist.length; i++) {
-			dist[i] = Integer.MAX_VALUE;
-		}
-		dist[_s] = 0;
-	}
-	
-	private static int minVertex (int [] _dist, boolean [] _v) {
-		int x = Integer.MAX_VALUE;
-		int y = -1;   // graph not connected, or no unvisited vertices
-		for (int i=0; i<_dist.length; i++) {
-			if (!_v[i] && _dist[i]<x) {y=i; x=_dist[i];}
-		}
-		return y;
-	}
-	*/
-	
 }
--- src/src/nf20/Main.java
+++ src/src/nf20/Main.java
@@ -1,12 +1,12 @@
 package nf20;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import structure.Arc;
-import structure.Graph;
-import structure.Node;
+import Algos.Dijkstra;
+import structure.*;
 
 public class Main {
 	 
@@ -18,18 +18,22 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<Arc> listArc		= new ArrayList<Arc>();
 		ArrayList<Node> listNode	= new ArrayList<Node>();
-		Scanner sc = new Scanner(System.in);
+		
+		Graph graphe;
+		Dijkstra dijkstra;
+		
+		//Scanner sc = new Scanner(System.in);
 		BufferedReader lectureFichier = null;
 		String url="";
 		String ligne;
-		Graph graphe;
 		boolean fichierLu = false;
 		
 		
 		while(!fichierLu){
 			//System.out.println("Lien Url vers votre fichier : ");
 			//url=sc.nextLine();
-			url="C:\\Users\\N33d-W33d\\Documents\\eclipse\\eclipse\\workspace\\nf20-choron-larue\\instance_PCC.dat";
+			//url="C:\\Users\\N33d-W33d\\Documents\\eclipse\\eclipse\\workspace\\nf20-choron-larue\\instance_PCC.dat";
+			url = "D:\\COURS\\LO02\\CrazyEights\\nf20-choron-larue\\instance_PCC.dat";
 			
 			String fichier="";
 			try{
@@ -47,18 +51,18 @@ public class Main {
 
 				String tableau[] = fichier.split("/");
 				
-				int nbrNodes = 0;
-				int nbrArcs = 0;
-				int indiceDepart =0;
+				int nbNodes			= 0;
+				int nbArcs			= 0;
+				int indiceDepart	= 0;
 			
 				
 				for(int i=0; i< tableau.length; i++){
 					if(tableau[i].equalsIgnoreCase("nb_nodes")){
-						nbrNodes = Integer.parseInt(tableau[i+1]);
+						nbNodes = Integer.parseInt(tableau[i+1]);
 						
 					}
 					else if(tableau[i].equalsIgnoreCase("nb_arcs")){
-						nbrArcs = Integer.parseInt(tableau[i+1]);
+						nbArcs = Integer.parseInt(tableau[i+1]);
 						
 					}
 					else if(tableau[i].endsWith("COSTS") || tableau[i].endsWith("COST")){
@@ -66,16 +70,16 @@ public class Main {
 					}
 				}
 				//creation des noeuds
-				for(int k=0; k<nbrArcs;k++){
+				for(int k=0; k<nbArcs; k++){
 					listNode.add(new Node(k));
 				}
 				
 				//Creation des arcs
-				for(int j=indiceDepart;j<tableau.length;j++){
+				for(int j=indiceDepart; j<tableau.length; j++){
 					if(tableau[j].equals("END")){
 						break;
 					}
-					//Tout les 3 elements
+					//Tous les 3 elements
 					if((j-indiceDepart)%3 == 0 || j-indiceDepart == 0){
 						//on crée un nouvel arc(node depart, node Arrivé, int cost)
 						//On l'ajoute à la listeArc
@@ -88,8 +92,8 @@ public class Main {
 				// On "dŽfinit" le graphe
 				graphe = Graph.getInstance();
 				
-				graphe.setNbNodes(nbrNodes);
-				graphe.setNbArcs(nbrArcs);
+				graphe.setNbNodes(nbNodes);
+				graphe.setNbArcs(nbArcs);
 				
 				graphe.setListArc(listArc);
 				graphe.setListNode(listNode);
@@ -103,6 +107,13 @@ public class Main {
 		for(Arc monArc : listArc){
 			System.out.println(monArc.toString());
 		}
+		
+		/*
+		 * TEST DIJKSTRA
+		 */
+		graphe = Graph.getInstance();
+		dijkstra = new Dijkstra(Graph.getInstance());
+		System.out.println(dijkstra.algoWikipedia(graphe.getListNode().get(1), graphe.getListNode().get(graphe.getNbNodes() -1)));
 		
 	}
 
